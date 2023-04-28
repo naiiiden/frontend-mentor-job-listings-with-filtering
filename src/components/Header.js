676ca7ad86c63e5f5a34a0e3def68409f076b95f
naiiiden <na4ko359@gmail.com>
2023-04-28 08:33:39 +0300

    render header if filter.length > 0

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,16 +6,20 @@ const Header = () => {
 
     return (
         <header>
-            <div className="header--form">
-                <label htmlFor="search" className="form--label">
-                    <span className="label--invisible">Search</span>
-                    <input id="search" type="text" className="text--input"/>
-                </label>
-                    <button className="clear--button" onClick={() => dispatch({ type: 'CLEAR' })}>Clear</button>
-            </div>
-            <div className="header-filter-board">
-                {filter.map(item => <button onClick={() => dispatch({ type: 'REMOVE_FILTER', filterItem: item})}>{item}</button>)}
-            </div>
+            {filter.length > 0 && (
+                <>
+                    <div className="header--form">
+                        <label htmlFor="search" className="form--label">
+                            <span className="label--invisible">Search</span>
+                            <input id="search" type="text" className="text--input"/>
+                        </label>
+                            <button className="clear--button" onClick={() => dispatch({ type: 'CLEAR' })}>Clear</button>
+                    </div>
+                    <div className="header-filter-board">
+                        {filter.map(item => <button onClick={() => dispatch({ type: 'REMOVE_FILTER', filterItem: item})}>{item}</button>)}
+                    </div>
+                </>
+            )}
         </header>
     )
 }
